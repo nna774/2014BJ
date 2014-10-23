399bdfa1482f93c85961945793b009c88fda07b1
NoNameA 774 <nonamea774@gmail.com>
2014-10-24 02:21:43 +0900

    report2

---
+++ report2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int fact(int n){
+    if(n == 0) return 1;
+    return n * fact(n-1);
+}
+
+int main(){
+    int n = 0;
+    scanf("%d", &n);
+    printf("%d! = %d", n, fact(n));
+    return 0;
+}
