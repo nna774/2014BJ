b2102dd850739f649627f2c967a24966f86dfc8c
NoNameA 774 <nonamea774@gmail.com>
2015-01-16 19:11:53 +0900

    iterate するように

--- report4-2/report4-2.c
+++ report4-2/report4-2.c
@@ -7,6 +7,7 @@
 #define SQARE(x) ((x) * (x))
 #define ABS(x) ((x) > 0 ? (x) : -(x))
 
+#define ITERATION 1000
 
 double f(double x) {
     return 2 * (1 + SQARE(x)) / (SQARE(1 - x * x) + SQARE(x));
@@ -20,13 +21,22 @@ double integral(int n) {
 
 int main() {
     double pi = atan(1.0) * 4;
-    init_gen_rand(12597);
+    double res[6];
+    for(int i=1; i < 7; ++i) res[i] = 0;
+    for(int it=0; it < ITERATION; ++it) {
+        init_gen_rand(12597+it);
+        int n = 1;
+        for(int i=1; i < 7; ++i) {
+            n *= 10;
+            res[i-1] += integral(n);
+        }
+    }
+    for(int i=1; i < 7; ++i) res[i] /= ITERATION;
     int n = 1;
     for(int i=1; i < 7; ++i) {
         n *= 10;
-        double res = integral(n);
         double e = 1.0 / sqrt(n);
-        printf("1e%d: %e error: %e l: %e\n", i, res, ABS(pi-res), ABS(pi-res)/ e);
+        printf("1e%d: %e error: %e l: %e\n", i, res[i-1], ABS(pi-res[i-1]), ABS(pi-res[i-1])/ e);
     }
     return 0;
 }
